3ccdac48c811ab90d32ffbe51cc9d9ff2a5cb56a
danieleBattistelli <bomber81@gmail.com>
2025-03-19 18:19:48 +0100

    Feat: Bonus

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Home');
+});
+
+Route::get('/blog', function () {
+    return view('Blog');
+});
+
+Route::get('/about', function () {
+    return view('About');
 });
